b3a7f4690d278014a272e02d302a255acced0e41
Max Nowack <max@unsou.de>
2019-02-24 01:16:44 +0100

    implement container startup delay

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import log from './log'
 import exec from './execute'
 import killSwitch from './killSwitch'
 import containerFinished from './containerFinished'
+import sleep from './sleep'
 
 const dropletId = process.env.DROPLET_ID
 const testRunId = process.env.TEST_RUN_ID
@@ -10,6 +11,7 @@ const dockerImage = process.env.DOCKER_IMAGE
 const dockerRegistry = process.env.DOCKER_REGISTRY
 const dockerUser = process.env.DOCKER_USER
 const dockerPass = process.env.DOCKER_PASS
+const startupDelay = parseInt(process.env.STARTUP_DELAY || '0', 10)
 const testVars = JSON.parse(process.env.TESTVARS || '{}')
 const instanceCount = parseInt(process.env.INSTANCE_COUNT || '1', 10)
 
@@ -19,6 +21,7 @@ const start = async () => {
   }
   await exec(`docker pull ${dockerImage}`)
   return Promise.all([...Array(instanceCount).keys()].map(async instanceNumber => {
+    await sleep(instanceNumber * startupDelay)
     const containerId = `${testRunId}-${dropletId}-${instanceNumber}`
     const vars = Object.keys(testVars).reduce((str, key) =>
       `${str} -e "${key}=${testVars[key]}"`, '')
